b30cd45f54aa43e46df1741ab6cd54171ea23086
AL1 <76146196+BecakMuda@users.noreply.github.com>
2021-05-02 21:08:00 +0700

    msm_thermal: update
    Idk bro just test

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -2337,7 +2337,8 @@ static ssize_t ocr_reg_mode_store(struct kobject *kobj,
 		pr_err("Invalid value %d for mode\n", val);
 		goto done_ocr_store;
 	}
-
+	    goto done_ocr_store;
+	    
 	if (val != reg->mode) {
 		ret = request_optimum_current(reg, val);
 		if (ret)
@@ -2431,6 +2432,7 @@ static ssize_t psm_reg_mode_store(struct kobject *kobj,
 		pr_err("Invalid number %d for mode\n", val);
 		goto done_psm_store;
 	}
+	    goto done_psm_store;
 
 	if (val != reg->mode) {
 		ret = rpm_regulator_set_mode(reg->reg, val);
@@ -3172,7 +3174,8 @@ static int __ref update_offline_cores(int val)
 
 	if (pend_hotplug_req && !in_suspend && !retry_in_progress) {
 		retry_in_progress = true;
-		schedule_delayed_work(&retry_hotplug_work,
+		queue_delayed_work(system_power_efficient_wq,
+			&retry_hotplug_work,
 			msecs_to_jiffies(HOTPLUG_RETRY_INTERVAL_MS));
 	}
 
@@ -3272,7 +3275,6 @@ static int __ref update_offline_cores(int val)
 {
 	return 0;
 }
-#endif
 
 static int do_gfx_phase_cond(void)
 {
@@ -3664,8 +3666,9 @@ static void check_temp(struct work_struct *work)
 
 reschedule:
 	if (polling_enabled)
-		schedule_delayed_work(&check_temp_work,
-				msecs_to_jiffies(msm_thermal_info.poll_ms));
+		queue_delayed_work(system_power_efficient_wq,
+			&check_temp_work,
+			msecs_to_jiffies(msm_thermal_info.poll_ms));
 }
 
 static int __ref msm_thermal_cpu_callback(struct notifier_block *nfb,
@@ -5003,6 +5006,7 @@ static ssize_t __ref store_cc_enabled(struct kobject *kobj,
 		pr_info("Core control enabled\n");
 		cpus_previously_online_update();
 		register_cpu_notifier(&msm_thermal_cpu_notifier);
+		goto done_store_cc;
 		/*
 		 * Re-evaluate thermal core condition, update current status
 		 * and set threshold for all cpus.
@@ -5057,6 +5061,7 @@ static ssize_t __ref store_cpus_offlined(struct kobject *kobj,
 		pr_err("Ignoring request; polling thread is enabled.\n");
 		goto done_cc;
 	}
+	    goto done_cc;
 
 	for_each_possible_cpu(cpu) {
 		if (!(msm_thermal_info.core_control_mask & BIT(cpu)))
